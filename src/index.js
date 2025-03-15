687005a406e6ea9c0e8b831c6e8f568aed8457a8
Naveen <naveen.yarramareddy@integra.co.in>
2025-03-15 16:27:51 +0530

    Added basic routing layer

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import { PORT } from './config/serverConfig.js';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', apiRouter);
+
 app.get('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({ message: 'pong' });
 });
